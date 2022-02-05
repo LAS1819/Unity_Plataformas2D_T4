a47c0810b024e6c8f2a49182a351d3ead47853d6
LXS_Elle <luxuszigard@gmail.com>
2022-02-05 12:50:03 +0100

    Added functionality to control game data between scenes (singleton)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,18 +7,27 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    // Objetos
+    private GameStatus gameStatus;
+
     // Miembros privados
     private int puntos; // Puntos del jugador
     private int vidas; // Vidas del jugador
+    private int nivelActual; // Nivel actual de juego
     private int objetosRestantes; // Número de objetos restantes para completar el nivel
 
     // Llamado en el primer frame
     void Start()
     {
-        // Inicialiazciones
-        puntos = 0;
-        vidas = 3;
+        // Obtenemos el objeto con el estado del juego
+        gameStatus = FindObjectOfType<GameStatus>();
+        // Obtenemos objetos restantes
         objetosRestantes = FindObjectsOfType<Objeto>().Length; // Objetos tipo Objeto
+
+        // Inicialiazciones mediante gameStatus
+        puntos = gameStatus.puntos;
+        vidas = gameStatus.vidas;
+        nivelActual = gameStatus.nivelActual;
 
         // Comprobamos los objetos que son estrellas
         // foreach (GameObject objetoRestante in objetosRestantes) {
@@ -45,6 +54,9 @@ public class GameController : MonoBehaviour
         // Sumamos 100 puntos
         puntos += 100;
 
+        // Añadimos puntos en el gameStatus
+        gameStatus.puntos = puntos;
+
         // TODO: Comprobar qué objeto se ha recogido para controlar sólo las estrellas
         // Restamos objeto
         objetosRestantes--;
@@ -64,6 +76,10 @@ public class GameController : MonoBehaviour
     // Método para restar una vida al jugador
     public void PerderVida() {
         vidas--; // Restamos una vida
+
+        // Pasamos las vidas al gameStatus
+        gameStatus.vidas = vidas;
+        
         // Obtenemos el objeto del jugador y madamos el mensaje Recolocar
         FindObjectOfType<Jugador>().SendMessage("Recolocar");
 
@@ -83,6 +99,16 @@ public class GameController : MonoBehaviour
 
     // Método para avanzar de nivel
     public void AvanzarNivel() {
-        SceneManager.LoadScene("Nivel 2");
+        // Sumamos un nivel
+        nivelActual++;
+        // Si hemos llegado al nivel máximo, volvemos al inicio
+        if (nivelActual > gameStatus.nivelMaximo) {
+            nivelActual = 1;
+        }
+        // Damos información del nivel a gameStatus
+        gameStatus.nivelActual = nivelActual;
+
+        // Cargamos nivel que toca
+        SceneManager.LoadScene("Nivel " + nivelActual);
     }
 }
--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,9 +1,31 @@
+// PMDM - T4 UNITY - PLATAFORMAS 2D - Lluís Aracil Sabater 2DAM 21/22
+// SCRIPT DEL COMPORTAMIENTO DE PUNTUACIONES, VIDAS y DEMÁS
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameStatus : MonoBehaviour
 {
+    // Métodos públicos
+    public int puntos = 0;
+    public int vidas = 3;
+    public int nivelActual = 1;
+    public int nivelMaximo = 2;
+
+    // Método AWAKE lanzado antes que el método START
+    private void Awake() {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+
+        // Si ya hay un gameStatus, lo destruimos
+        if (gameStatusCount > 1) {
+            Destroy(gameObject);
+        }
+        // Sino, no lo destruimos
+        else {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
